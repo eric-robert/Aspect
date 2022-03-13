db881b2a825d8fa7906407aad6b9f30ea9fcdd84
eric-robert <services@ericrobert.me>
2022-03-12 19:04:18 -0500

    with builder

--- src/client.ts
+++ src/client.ts
@@ -1,13 +1,21 @@
 import { AspectEngine } from "./engine/Engine";
+import { ModuleBuilder } from "./engine/Engine.types";
 import { HandshakeModule } from "./engine/modules/Handshake.module";
+import { EngineModule } from "./engine/modules/Module";
 import { ClientModule } from "./modules/client/Client";
 
-export default function ( ) {
+interface construct {
+    modules ?: ModuleBuilder<EngineModule>[]
+    handshake ?: ModuleBuilder<HandshakeModule>
+}
+
+export default function ( construct : construct ) {
     return new AspectEngine({
         modules : [
-            ClientModule
+            ClientModule,
+            ...(construct.modules || [])
         ],
-        handshakeModule : HandshakeModule,
+        handshakeModule : construct.handshake || HandshakeModule,
         settings : {}
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import { AspectEngine } from "./engine/Engine";
+import { ModuleBuilder } from "./engine/Engine.types";
 import { HandshakeModule } from "./engine/modules/Handshake.module";
+import { EngineModule } from "./engine/modules/Module";
 import { ServerModule } from "./modules/server/Server";
 
-export default function ( ) {
+interface construct {
+    modules ?: ModuleBuilder<EngineModule>[]
+    handshake ?: ModuleBuilder<HandshakeModule>
+}
+
+export default function ( construct : construct ) {
     return new AspectEngine({
         modules : [
-            ServerModule
+            ServerModule,
+            ...(construct.modules || [])
         ],
-        handshakeModule : HandshakeModule,
+        handshakeModule : construct.handshake || HandshakeModule,
         settings : {}
     })
 }
